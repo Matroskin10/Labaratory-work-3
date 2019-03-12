908eb7eb0b77b57db8bdf607096b41a1d8a87b7e
ArkhipovNA <n.arkhipov1@yandex.ru>
2019-03-12 16:27:20 +0500

    Add cout in ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -9,6 +9,7 @@ int main()
 std::cout << "The purpose of the course-the study of C++\n";
 std::cout << "Performance of the third laboratory work\n";
 std::cout << "Work with Branch!!!\n";
+std::cout << "How does this work?!\n";
 	system("pause");
 }
 
